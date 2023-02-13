c34cdf8cd1fa963e9c0539eae70a97db84f553dd
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-13 12:52:46 +0300

    deletePost

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import NewPost from "./pages/NewPost";
 function App() {
   const URL = "http://localhost:7777/posts";
   const [posts, setPosts] = useState([]);
+  const [render, setRender] = useState(0);
 
   useEffect(() => {
     fetch(URL)
@@ -16,15 +17,29 @@ function App() {
       .then((posts) => setPosts(posts))
       .catch((error) => console.log(error))
       .finally(console.log("finnaly"));
-  }, []);
+  }, [render]);
+
+  const deletePost = (id) => {
+    fetch(URL + "/" + id, {
+      method: "DELETE",
+    })
+      .then()
+      .then()
+      .catch()
+      .finally(console.log("Удаляем пост", id));
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home posts={posts} />} />
           <Route path="/posts/new" element={<NewPost />} />
-          <Route path="/posts/:id" element={<ViewPost posts={posts} />} />
-          {/*    <Route path="/posts/edit:id" element={EditPost} /> */}
+          <Route
+            path="/posts/:id"
+            element={<ViewPost posts={posts} handleDeletePost={deletePost} />}
+          />
+          {/*<Route path="/posts/edit:id" element={EditPost} /> */}
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,19 +1,30 @@
 import Post from "../components/Post";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Button from "../UI/Button";
 import "./ViewPost.scss";
 
-function ViewPost({ posts }) {
+function ViewPost({ posts, handleDeletePost }) {
   // Вытаскиваю из массива постов пост с ID
   const params = useParams();
   const post = posts.find((post) => post.id === +params.id);
+  // console.log(post.id);
+  const handleChangePost = () => {
+    console.log("Нажали кнопку изменить пост");
+  };
+  const navigate = useNavigate();
 
   return (
     <div>
       <Post {...post} />
       <div className="buttons">
-        <Button text="Изменить" />
-        <Button text="Удалить" />
+        <Button onClick={handleChangePost} text="Изменить" />
+        <Button
+          onClick={() => {
+            handleDeletePost(post.id);
+            navigate("/");
+          }}
+          text="Удалить"
+        />
       </div>
     </div>
   );
